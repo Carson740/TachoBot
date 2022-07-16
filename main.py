bc2dd81df3625cb4229365605881d55e5fb77e86
Carson740 <carjhunt3@gmail.com>
2022-07-16 13:45:28 -0500

    First Iteration of Tachobot

---
+++ main.py
@@ -0,0 +1,10 @@
+import hikari
+
+bot = hikari.GatewayBot(token='OTk3ODc0OTMzMjk0MTc0MzU5.GAGC0h.id5-zr6NRoLHoTWKHde1oMBNjh2kd-fx7caqf0')
+
+@bot.listen(hikari.GuildMessageCreateEvent)
+async def print_message(event):
+    print(event.content)
+
+
+bot.run()
