c37135cd3a2daef29504f2cb4da392846987bd7d
Carson740 <carjhunt3@gmail.com>
2022-07-16 15:44:30 -0500

    First Iteration of Tachobot

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-import hikari
-
-bot = hikari.GatewayBot(token='OTk3ODc0OTMzMjk0MTc0MzU5.GAGC0h.id5-zr6NRoLHoTWKHde1oMBNjh2kd-fx7caqf0')
-print("start")
-@bot.listen(hikari.GuildMessageCreateEvent)
-async def print_message(event):
-    print(event.content)
+import discord
 
 
-bot.run()
+class MyClient(discord.Client):
+    async def on_ready(self):
+        print('Logged on as', self.user)
+
+    async def on_message(self, message):
+        # don't respond to ourselves
+        if message.author == self.user:
+            return
+
+        if message.content == '!ping':
+            await message.channel.send('bruh')
+
+        if message.content.startswith("!cast"):
+            channel = client.get_channel(997844570299711492)
+            user = message.author.id
+            cast = 997673917789519933
+            print(message.content)
+            echo = message.content.replace('!cast', '')
+            msg = f"**<@{user}> has requested <@&{cast}>!\n\nIf you would like to apply to be cast, reply in the message thread!**" + "```" + echo + "```"
+            await channel.send(msg)
+
+
+client = MyClient()
+client.run('OTk3ODc0OTMzMjk0MTc0MzU5.GAGC0h.id5-zr6NRoLHoTWKHde1oMBNjh2kd-fx7caqf0')
